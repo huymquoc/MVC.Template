a79cb2248d6760109f9d89fec75b3e41eede9d1b
huymquoc <huymquoc@gmail.com>
2014-12-17 09:17:58 +0700

    second changes

--- WebFrameWork/Global.asax
+++ WebFrameWork/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebFrameWork.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC.Template.Web.MvcApplication" Language="C#" %>
--- WebFrameWork/Migrations/Configuration.cs
+++ WebFrameWork/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+using MVC.Template.Web.Models;
+
 namespace MVC.Template.Web.Migrations
 {
     using System;
@@ -5,14 +7,14 @@ namespace MVC.Template.Web.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<MVC.Template.Web.Domain.FrameWorkDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false ;
         }
 
-        protected override void Seed(MVC.Template.Web.Domain.FrameWorkDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
